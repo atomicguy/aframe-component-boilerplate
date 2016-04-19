1ee88a3af30816b7d86e5c6808975d645d990b93
atomicguy <adam@atompowered.net>
2016-04-19 11:21:44 -0400

    update with x0 and z0
    added ability to set an initial (x,z) corner to position the fence
    where needed in a scene

--- index.js
+++ index.js
@@ -7,28 +7,52 @@ if (typeof AFRAME === 'undefined') {
  */
 AFRAME.registerComponent('fence', {
   schema: {
-      width: {default: 10},
-      depth: {default: 10}
+      width: {
+          type: 'number',
+          default: 10
+      },
+      depth: {
+          type: 'number',
+          default: 10
+      },
+      x0: {
+          type: 'number',
+          default: 0
+      },
+      z0: {
+          type: 'number',
+          default: 0
+      }
   },
 
   /**
    * Called once when component is attached. Generally for initial setup.
    */
-  init: function () {},
+  init: function () {
+
+    },
+
 
   tick: function() {
       var data = this.data;
       var thiswidth = data.width;
       var thisdepth = data.depth;
+      var x0 = data.x0;
+      var z0 = data.z0;
+
+      minX = thiswidth / 2 + x0;
+      maxX = ( -1 * thiswidth / 2 ) + x0;
+
+      minZ = thisdepth / 2 + z0;
+      maxZ = ( -1 * thisdepth / 2 ) + z0;
 
       var position = this.el.getComputedAttribute('position');
 
+      position.x = Math.min( minX, position.x);
+      position.x = Math.max( maxX, position.x);
 
-      position.x = Math.min( thiswidth / 2, position.x);
-      position.x = Math.max( -1* thiswidth / 2, position.x);
-
-      position.z = Math.min( thisdepth / 2, position.z);
-      position.z = Math.max( -1* thisdepth / 2, position.z);
+      position.z = Math.min( minZ, position.z);
+      position.z = Math.max( maxZ, position.z);
 
       this.el.setAttribute('position', position);
   },
@@ -37,7 +61,27 @@ AFRAME.registerComponent('fence', {
    * Called when component is attached and when component data changes.
    * Generally modifies the entity based on the data.
    */
-  update: function (oldData) { },
+  update: function (oldData) {
+      console.log(this.el);
+      var data = this.data;
+      console.log(data);
+
+      var thiswidth = data.width;
+      var thisdepth = data.depth;
+      var x0 = data.x0;
+      var z0 = data.z0;
+
+      minX = thiswidth / 2 - x0;
+      maxX = ( -1 * thiswidth / 2 ) - x0;
+
+      minZ = thisdepth / 2 + z0;
+      maxZ = ( -1 * thisdepth / 2 ) - z0;
+
+      console.log(minX);
+      console.log(maxX);
+      console.log(minZ);
+      console.log(maxZ);
+  },
 
   /**
    * Called when a component is removed (e.g., via removeAttribute).
