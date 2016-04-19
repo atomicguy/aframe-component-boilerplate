cf2d5972ca172a3b72e0c3b809a5c90ae1fc3ee4
atomicguy <adam@atompowered.net>
2016-04-19 11:25:20 -0400

    update dist

--- dist/aframe-fence-component.js
+++ dist/aframe-fence-component.js
@@ -71,13 +71,6 @@
 	      }
 	  },
 
-	  /**
-	   * Called once when component is attached. Generally for initial setup.
-	   */
-	  init: function () {
-
-	    },
-
 
 	  tick: function() {
 	      var data = this.data;
@@ -103,54 +96,6 @@
 	      this.el.setAttribute('position', position);
 	  },
 
-	  /**
-	   * Called when component is attached and when component data changes.
-	   * Generally modifies the entity based on the data.
-	   */
-	  update: function (oldData) {
-	      console.log(this.el);
-	      var data = this.data;
-	      console.log(data);
-
-	      var thiswidth = data.width;
-	      var thisdepth = data.depth;
-	      var x0 = data.x0;
-	      var z0 = data.z0;
-
-	      minX = thiswidth / 2 - x0;
-	      maxX = ( -1 * thiswidth / 2 ) - x0;
-
-	      minZ = thisdepth / 2 + z0;
-	      maxZ = ( -1 * thisdepth / 2 ) - z0;
-
-	      console.log(minX);
-	      console.log(maxX);
-	      console.log(minZ);
-	      console.log(maxZ);
-	  },
-
-	  /**
-	   * Called when a component is removed (e.g., via removeAttribute).
-	   * Generally undoes all modifications to the entity.
-	   */
-	  remove: function () { },
-
-	  /**
-	   * Called on each scene tick.
-	   */
-	  // tick: function (t) { },
-
-	  /**
-	   * Called when entity pauses.
-	   * Use to stop or remove any dynamic or background behavior such as events.
-	   */
-	  pause: function () { },
-
-	  /**
-	   * Called when entity resumes.
-	   * Use to continue or add any dynamic or background behavior such as events.
-	   */
-	  play: function () { },
 	});
 
 
