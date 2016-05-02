a682427844c1e0638c78c42bd260c24e539680c6
atomicguy <adam@atompowered.net>
2016-05-02 13:28:43 -0400

    updated dist files

--- dist/aframe-fence-component.js
+++ dist/aframe-fence-component.js
@@ -45,11 +45,16 @@
 /***/ function(module, exports) {
 
 	if (typeof AFRAME === 'undefined') {
-	  throw 'Component attempted to register before AFRAME was available.';
+	  throw new Error('Component attempted to register before AFRAME was available.');
 	}
 
 	/**
 	 * Fence component for A-Frame.
+	 *
+	 * Thanks to Don McCurdy <https://github.com/donmccurdy>
+	 * for his initial code
+	 * and ngokevin's Component Boilerplate <https://github.com/ngokevin/aframe-component-boilerplate>
+	 *
 	 */
 	AFRAME.registerComponent('fence', {
 	  schema: {
